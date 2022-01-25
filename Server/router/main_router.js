2e7877b3c3d5833611a4fa58dbe249ea2930df36
DESKTOP-UL97IHH\admin <dbc200188@gmail.com>
2022-01-26 08:12:37 +0900

    0126 update

--- Server/router/main_router.js
+++ Server/router/main_router.js
@@ -1,14 +1,80 @@
 const axios = require('axios');
 const e = require('express');
 
-let riotAPI_Key = 'RGAPI-4091d591-9c7d-43ac-b637-3178ef737577';
+let riotAPI_Key = 'RGAPI-aa2781c8-4edf-492c-896c-c7ffd21be849';
 let riotVerision = '12.1.1';
-let champion_Data = null;
-const champion_Data_Array = {};
-const item_Data_Array = {};
-let item_Data = null;
-let item_child_data = null;
-const champion_detail_data_array = {};
+let champion_Data_Array = {};
+let item_Data_Array = {};
+let champion_detail_data_array = {};
+let summoner_Array = {};
+let rune_Array = {};
+
+const stats = [
+    {
+        id: 5001,
+        name: 'HealthScaling',
+        majorChangePatchVersion: '',
+        tooltip: '체력 +@f1@ (레벨에 비례)',
+        shortDesc: '체력 +15~140 (레벨에 비례)',
+        longDesc: '체력 +15~140 (레벨에 비례)',
+        icon: 'perk-images/StatMods/StatModsHealthScalingIcon.png',
+        endOfGameStatDescs: [],
+    },
+    {
+        id: 5002,
+        name: 'Armor',
+        majorChangePatchVersion: '',
+        tooltip: '방어력 +6',
+        shortDesc: '방어력 +6',
+        longDesc: '방어력 +6',
+        icon: 'perk-images/StatMods/StatModsArmorIcon.png',
+        endOfGameStatDescs: [],
+    },
+    {
+        id: 5003,
+        name: 'MagicRes',
+        majorChangePatchVersion: '',
+        tooltip: '마법 저항력 +8',
+        shortDesc: '마법 저항력 +8',
+        longDesc: '마법 저항력 +8',
+        icon: 'perk-images/StatMods/StatModsMagicResIcon.png',
+        endOfGameStatDescs: [],
+    },
+    {
+        id: 5007,
+        name: 'CDRScaling',
+        majorChangePatchVersion: '',
+        tooltip: '스킬 가속 +@f1@',
+        shortDesc:
+            "<lol-uikit-tooltipped-keyword key='LinkTooltip_Description_CDR'>스킬 가속</lol-uikit-tooltipped-keyword> +8 ",
+        longDesc:
+            "<lol-uikit-tooltipped-keyword key='LinkTooltip_Description_CDR'>스킬 가속</lol-uikit-tooltipped-keyword> +8 ",
+        icon: 'perk-images/StatMods/StatModsCDRScalingIcon.png',
+        endOfGameStatDescs: [],
+    },
+    {
+        id: 5005,
+        name: 'AttackSpeed',
+        majorChangePatchVersion: '',
+        tooltip: '공격 속도 +10%',
+        shortDesc: '공격 속도 +10%',
+        longDesc: '공격 속도 +10%',
+        icon: 'perk-images/StatMods/StatModsAttackSpeedIcon.png',
+        endOfGameStatDescs: [],
+    },
+    {
+        id: 5008,
+        name: 'Adaptive',
+        majorChangePatchVersion: '',
+        tooltip: '<scaleAD>+공격력 @f2@</scaleAD>',
+        shortDesc:
+            "<lol-uikit-tooltipped-keyword key='LinkTooltip_Description_Adaptive'><font color='#48C4B7'>적응형 능력치</font></lol-uikit-tooltipped-keyword> +9",
+        longDesc:
+            "<lol-uikit-tooltipped-keyword key='LinkTooltip_Description_Adaptive'><font color='#48C4B7'>적응형 능력치</font></lol-uikit-tooltipped-keyword> +9",
+        icon: 'perk-images/StatMods/StatModsAdaptiveForceIcon.png',
+        endOfGameStatDescs: [],
+    },
+];
 
 const makeChild = (list, itemDB) => {
     const list_data = list.map(item => {
@@ -64,58 +130,6 @@ const get_Init_Data = async () => {
     } catch (err) {
         res.json(err);
     }
-    //     const champion = await axios.get(
-    //         `http://ddragon.leagueoflegends.com/cdn/${version.data[0]}/data/ko_KR/champion.json`,
-    //         {
-    //             headers: {
-    //                 'X-Riot-Token': riotAPI_Key,
-    //             },
-    //         }
-    //     );
-    //     const itemdata = await axios.get(
-    //         `http://ddragon.leagueoflegends.com/cdn/${version.data[0]}/data/ko_KR/item.json`,
-    //         {
-    //             headers: {
-    //                 'X-Riot-Token': riotAPI_Key,
-    //             },
-    //         }
-    //     );
-
-    //     let fchampion_Data = Object.entries(champion.data.data).map(([key, value], index) => {
-    //         delete value.info;
-    //         delete value.partype;
-    //         delete value.stats;
-    //         delete value.version;
-    //         value.num = index;
-
-    //         return [key, value];
-    //     });
-
-    //     champion_Data = fchampion_Data.sort((a, b) => {
-    //         if (a[1].name < b[1].name) return -1;
-    //         else if (a[1].name == b[1].name) return 0;
-    //         else return 1;
-    //     });
-    //     item_Data = itemdata.data.data;
-
-    //     const itemList = [];
-    //     for (let item in item_Data) {
-    //         itemList.push({
-    //             key: item,
-    //             legendary:
-    //                 itemdata.data.groups.findIndex(group => group.id === item && group.MaxGroupOwnable === '1') !== -1
-    //                     ? true
-    //                     : false,
-    //             mythic: item_Data[item].description.includes('rarityMythic') ? true : false,
-
-    //             ...item_Data[item],
-    //         });
-    //     }
-
-    //     item_child_data = makeChild(itemList, item_Data);
-    // } catch (error) {
-    //     console.log(error);
-    // }
 };
 
 module.exports = app => {
@@ -129,7 +143,10 @@ module.exports = app => {
     app.post('/refreshVersion', (req, res) => {
         riotVerision = req.body.version;
         champion_Data_Array = {};
+        champion_detail_data_array = {};
         item_Data_Array = {};
+        summoner_Array = {};
+        rune_Array = {};
         console.log(riotVerision);
         res.json({ version: riotVerision });
     });
@@ -145,6 +162,7 @@ module.exports = app => {
         let summoner_Rank_Data = [];
         let Summoner_Match_List = [];
         let Summoner_Match_Data = [];
+        let Summoner_Active_GAME = {};
 
         if (req.body.type) {
             type = req.body.type;
@@ -164,7 +182,23 @@ module.exports = app => {
                     summoner_Basic_Data = response.data;
                 });
 
-            await axios
+            axios
+                .get(
+                    'https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/' + summoner_Basic_Data.id,
+                    {
+                        headers: {
+                            'X-Riot-Token': riotAPI_Key,
+                        },
+                    }
+                )
+                .then(response => {
+                    Summoner_Active_GAME = response.data;
+                })
+                .catch(error => {
+                    Summoner_Active_GAME = { message: 'NODATA', error: error.message };
+                });
+
+            axios
                 .get('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summoner_Basic_Data.id, {
                     headers: {
                         'X-Riot-Token': riotAPI_Key,
@@ -199,7 +233,37 @@ module.exports = app => {
                                     'X-Riot-Token': riotAPI_Key,
                                 },
                             })
-                            .then(response => {
+                            .then(async response => {
+                                let match = response.data;
+
+                                if (match.info.queueId === 420) {
+                                    const team = [null, null, null, null, null, null, null, null, null, null];
+
+                                    await match.info.participants.map((user, index) => {
+                                        if (user.teamPosition !== '') {
+                                            switch (user.teamPosition) {
+                                                case 'TOP':
+                                                    user.teamId === 100 ? (team[0] = user) : (team[5] = user);
+                                                    break;
+                                                case 'JUNGLE':
+                                                    user.teamId === 100 ? (team[1] = user) : (team[6] = user);
+                                                    break;
+                                                case 'MIDDLE':
+                                                    user.teamId === 100 ? (team[2] = user) : (team[7] = user);
+                                                    break;
+                                                case 'BOTTOM':
+                                                    user.teamId === 100 ? (team[3] = user) : (team[8] = user);
+                                                    break;
+                                                case 'UTILITY':
+                                                    user.teamId === 100 ? (team[4] = user) : (team[9] = user);
+                                                    break;
+                                            }
+                                        }
+                                    });
+
+                                    match.info.participants = team;
+                                }
+
                                 Summoner_Match_Data.push(response.data);
                             });
                     } catch (error) {
@@ -212,12 +276,14 @@ module.exports = app => {
                 Summoner_Basic_Data: summoner_Basic_Data,
                 Summoner_Rank_Data: summoner_Rank_Data,
                 Summoner_Match_Data: Summoner_Match_Data,
+                Summoner_Active_GAME: Summoner_Active_GAME,
             });
         } catch (error) {
             console.log(error);
             res.json({ search_Error: error });
         }
     });
+
     app.post('/multi_search', async (req, res) => {
         const summonerName_array = req.body.summonerName_array;
         let summoner_Array_Data = [];
@@ -287,6 +353,7 @@ module.exports = app => {
                                     },
                                 })
                                 .then(response => {
+                                    console.log(response.data);
                                     Summoner_Match_Data.push(response.data);
                                 });
                         } catch (error) {
@@ -452,6 +519,48 @@ module.exports = app => {
             }
 
             res.json(item_Data_Array[req.query.lang]);
+        } catch (error) {
+            res.json(error);
+        }
+    });
+
+    app.get('/summoner', async (req, res) => {
+        try {
+            if (!summoner_Array[req.query.lang]) {
+                const { data } = await axios.get(
+                    `http://ddragon.leagueoflegends.com/cdn/${riotVerision}/data/${req.query.lang}/summoner.json`,
+                    {
+                        headers: {
+                            'X-Riot-Token': riotAPI_Key,
+                        },
+                    }
+                );
+                summoner_Array[req.query.lang] = data;
+            }
+
+            res.json(summoner_Array[req.query.lang]);
+        } catch (error) {
+            res.json(error);
+        }
+    });
+
+    app.get('/rune', async (req, res) => {
+        try {
+            if (!rune_Array[req.query.lang]) {
+                const { data } = await axios.get(
+                    `http://ddragon.leagueoflegends.com/cdn/${riotVerision}/data/${req.query.lang}/runesReforged.json`,
+                    {
+                        headers: {
+                            'X-Riot-Token': riotAPI_Key,
+                        },
+                    }
+                );
+
+                data.push(...stats);
+                rune_Array[req.query.lang] = data;
+            }
+
+            res.json(rune_Array[req.query.lang]);
         } catch (error) {
             res.json(error);
         }
